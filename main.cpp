641e0e7ac47a26f67049241819890b838b0bf4ea
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-04-01 16:05:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
+  int n, i, a=0, b=1, risultato=0;
+    cin >> n;
+    if (n<2){
+        cout << "errore";
+    }else{
+        for(i=0;i<n;i++){
+        risultato=a+b;
+        b=a;
+        a=risultato;
+        cout << risultato <<endl;
+        }
+    }
 int main() {
    return 0;
 }
